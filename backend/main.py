c721deee24af831808aececcd20110691f05d249
patildhrup <patildhrup0123@gmail.com>
2026-06-03 09:51:18 +0530

    backend:  yml file updated

--- backend/main.py
+++ backend/main.py
@@ -32,10 +32,3 @@ async def hello():
         "message": "Hello from FastAPI"
     }
 
-# Render / Production startup
-if __name__ == "__main__":
-    uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 8000))
-    )
